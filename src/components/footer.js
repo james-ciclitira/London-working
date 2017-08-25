e82cb03d2932cc4e892d3bbe5ff0a25ed50a4abb
james-ciclitira <jciclitira@gmail.com>
2017-08-25 13:42:42 +0100

    old george

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 class Footer extends React.Component {
   render() {
     return (
-<section className='mt7'>
+<section className='mt4'>
 <footer className="pv4 ph3 ph5-ns tc">
   <small className="f7 db pb2 tc">Made by <a href='https://twitter.com/jimmmy'>jimmmy</a> using Tachyons.io, Reactjs, NetlifyCMS</small>
   <small className="f6 db pb3 tc">© 2017, ALL RIGHTS RESERVED</small>
