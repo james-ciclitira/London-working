8c448d437ee1f11ee7da0e4dd8b461afed9a3990
james-ciclitira <jciclitira@gmail.com>
2017-08-26 13:08:22 +0100

    new footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return (
 <section className='mt4'>
 <footer className="pv4 ph3 ph5-ns tc">
-  <small className="f7 db pb2 tc">Made by <a href='https://twitter.com/jimmmy'>jimmmy</a> using Tachyons.io, Reactjs, NetlifyCMS</small>
+  <small className="f7 db pb2 tc">Made by <a target='blank' href='https://twitter.com/jimmmy'>jimmy</a> using Tachyons.io, Reactjs, NetlifyCMS</small>
   <small className="f6 db pb3 tc">© 2017, ALL RIGHTS RESERVED</small>
 </footer>
 </section>
---
+++ src/pages/blog/2017-08-25-arepaandco.md
@@ -0,0 +1,11 @@
+---
+date: 2017-08-25T17:37:00.248Z
+title: Arepa&CO
+type: Restaurant
+wifi: N/A
+password: N/A
+hours: '8:00-22:30'
+directions: Na
+image: /imgaes/IMG_20170825_183346.jpg
+---
+
